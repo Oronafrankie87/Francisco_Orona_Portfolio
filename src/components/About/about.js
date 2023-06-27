5ac81a5449b6e35fc82a02cc7feb8f9eb6a45a84
Francisco Orona <oronafrankie87@gmail.com>
2023-06-27 17:34:31 -0600

    Grammer and Spelling updates

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -9,24 +9,27 @@ function About() {
     <section className="page-container">
       <div className="about-2">
         <div className="image-container">
-          <img className="profilePic" src={profilePic} alt="My Profile pic drawing"></img>
+          <img
+            className="profilePic"
+            src={profilePic}
+            alt="My Profile pic drawing"
+          ></img>
         </div>
         <div className="text-container">
           <h2>About Me</h2>
           <p>
-            Hello, my name is Francisco Orona but my family and friend know me
-            as Frankie. As a skilled software developer with, I have a diverse
+            Hello, my name is Francisco Orona, but my family and friends know me
+            as Frankie. I am a skilled software developer with a diverse
             background that fuels my creativity.
           </p>
           <p>
-            Before venturing into the world of technology, I embarked on an
-            exciting musical journey. For several years, I toured and worked as
-            a studio musician in Los Angeles, showcasing my talent on guitar,
-            piano, bass and songwriting. I immersed myself in a wide range of
-            music genres, from rock, country, and blues to pop, hip hop, and
-            jazz. This experience allowed me to develop a deep appreciation for
-            the power of artistic expression and its ability to connect with
-            people.
+            Before entering the world of technology, I embarked on an exciting
+            musical journey. For several years, I toured and worked as a studio
+            musician in Los Angeles, showcasing my talent on guitar, piano, bass
+            and songwriting. I immersed myself in a wide range of music genres,
+            from rock, country, and blues to pop, hip hop, and jazz. This
+            experience allowed me to develop a deep appreciation for the power
+            of artistic expression and its ability to connect with people.
           </p>
           <p>
             In parallel to my musical pursuits, I discovered an abiding passion
@@ -38,21 +41,21 @@ function About() {
             description which was made using Excalidraw.
           </p>
           <p>
-           Drawing from my previous roles as a pharmacy technician and a business
-           owner in medical billing, I've had the opportunity to develop a range
-           of valuable qualities that greatly contribute to my work as a software
-           developer. These qualities encompass my ability to handle challenging
-           situations with composure and dedication, my inclination towards
-           taking initiative and being self-motivated, a genuine empathy for
-           other, and a strong commitment to precision and thoroughness. With my
-           diverse skill set and the integration of music, graphic design, and
-           technology, I strive to approach projects with a humble yet
-           imaginative perspective
-           </p>
+            Drawing from my previous roles as a pharmacy technician and a
+            business owner in medical billing, I have developed valuable
+            qualities that greatly contribute to my work as a software
+            developer. These qualities include the ability to handle challenging
+            situations with composure and dedication, a proactive and
+            self-motivated approach, genuine empathy for others, and a strong
+            commitment to precision and thoroughness. With my diverse skill set
+            and the integration of music, graphic design, and technology, I
+            strive to approach projects with a humble yet imaginative
+            perspective.
+          </p>
           <p>
             Combining my technical expertise, musical abilities, and artistic
             sensibilities, I endeavor to deliver innovative solutions that
-            surpass expectations. By blending my creativity, proficiency in
+            exceed expectations. By blending my creativity, proficiency in
             multiple instruments, diverse understanding of genres, and
             technological prowess, I offer a distinctive perspective and
             contribute value to each project. As a software developer with a
